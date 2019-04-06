a5d22a11884916f4a44537edb17822fa4666fad4
William Lin 22 <ganlanyuan@users.noreply.github.com>
2019-04-06 13:09:45 -0400

    seperate CSS from JS

--- index.js
+++ index.js
@@ -1,73 +1,20 @@
-void (function (root, factory) {
-  if (typeof define === 'function' && define.amd) define(factory)
-  else if (typeof exports === 'object') module.exports = factory()
-  else factory()
-}(this, function () {
-  var DETAILS = 'details'
-  var SUMMARY = 'summary'
+var UglifyJS = require('uglify-js');
 
-  var supported = checkSupport()
-  if (supported) return
+var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, 'details-polyfill.js');
 
-  // Add a classname
-  document.documentElement.className += ' no-details'
-
-  window.addEventListener('click', clickHandler)
-
-  injectStyle('details-polyfill-style',
-    'html.no-details ' + DETAILS + ':not([open]) > :not(' + SUMMARY + ') { display: none; }\n' +
-    'html.no-details ' + DETAILS + ' > ' + SUMMARY + ':before { content: "\u25b6"; display: inline-block; font-size: .8em; width: 1.5em; }\n' +
-    'html.no-details ' + DETAILS + '[open] > ' + SUMMARY + ':before { content: "\u25bc"; }')
-
-  /*
-   * Click handler for `<summary>` tags
-   */
-
-  function clickHandler (e) {
-    if (e.target.nodeName.toLowerCase() === 'summary') {
-      var details = e.target.parentNode
-      if (!details) return
-
-      if (details.getAttribute('open')) {
-        details.open = false
-        details.removeAttribute('open')
-      } else {
-        details.open = true
-        details.setAttribute('open', 'open')
-      }
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+    if (!err) {
+      // console.log('received data: ' + data);
+      // response.writeHead(200, {'Content-Type': 'text/html'});
+      // response.write(data);
+      // response.end();
+      fs.writeFile('details-polyfill.min.js', UglifyJS.minify(data).code, function(err) {
+          if(err) { return console.log(err); }
+          console.log('details-polyfill.js was minified!');
+      }); 
+    } else {
+        console.log(err);
     }
-  }
-
-  /*
-   * Checks for support for `<details>`
-   */
-
-  function checkSupport () {
-    var el = document.createElement(DETAILS)
-    if (!('open' in el)) return false
-
-    el.innerHTML = '<' + SUMMARY + '>a</' + SUMMARY + '>b'
-    document.body.appendChild(el)
-
-    var diff = el.offsetHeight
-    el.open = true
-    var result = (diff != el.offsetHeight)
-
-    document.body.removeChild(el)
-    return result
-  }
-
-  /*
-   * Injects styles (idempotent)
-   */
-
-  function injectStyle (id, style) {
-    if (document.getElementById(id)) return
-
-    var el = document.createElement('style')
-    el.id = id
-    el.innerHTML = style
-
-    document.getElementsByTagName('head')[0].appendChild(el)
-  }
-})); // eslint-disable-line semi
+});
